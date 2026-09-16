188cc2916f826219449b9992905a30c872bf8585
Timani Abubakar <timaniabubakar@gmail.com>
2026-09-15 17:38:05 -0700

    Add cron job for health check and update server to start it in production

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -5,22 +5,30 @@ import { clerkMiddleware } from "@clerk/express";
 import { clerkWebhookHandler } from "./webhooks/clerk";
 import { getEnv } from "./lib/env";
 import "dotenv/config";
+import keepAliveCron from "./lib/cron"
+
 
 import fs from "node:fs";
 import path from "node:path";
 
-
-const rawJson = express.raw({ type: "application/json", limit: "1mb" });
-
 const app = express();
 const env = getEnv();
 
-app.use(cors());
-app.use(clerkMiddleware());
+
+const rawJson = express.raw({ type: "application/json", limit: "1mb" });
 
 app.post("/webhooks/clerk", rawJson, (req, res) => {
   void clerkWebhookHandler(req, res);
 });
+
+app.use(express.json())
+app.use(cors());
+app.use(clerkMiddleware());
+
+app.get("/health", (_req, res) => {
+  res.json({ok:true});
+})
+
 
 const publicDir = path.join(process.cwd(), "public");
 if (fs.existsSync(publicDir)) {
@@ -41,4 +49,9 @@ if (fs.existsSync(publicDir)) {
   });
 }
 
-app.listen(env.PORT, () => console.log("listening on port:", env.PORT))
+app.listen(env.PORT, () => {
+  console.log("Listening on port:", env.PORT);
+  if (env.NODE_ENV === "production") {
+    keepAliveCron.start();
+  }
+});
